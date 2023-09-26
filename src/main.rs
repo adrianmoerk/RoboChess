a63f10310ac5e7748749c125271382250d021d1f
Adrian Mörk <adrianmoerk@protonmail.com>
2023-09-26 11:47:02 +0200

    TEST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut stream = TcpStream::connect("ROBOTER_IP:30002").await?; // Port 30002 ist für URScript-Befehle
+    let move_command = "movej([0, -1.57, 0, -1.57, 0, 0], a=1.2, v=0.3)\n"; // Beispielbefehl
+    stream.write_all(move_command.as_bytes()).await?;
+    Ok(())
 }
