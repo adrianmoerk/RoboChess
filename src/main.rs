1f22f9db79596f179ca3c13ac7300126436b1639
Max <maxsunkovsky@gmail.com>
2023-10-04 11:21:29 +0200

    Geschwindigkeit per Command einstellbar; UNTESTED!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod robot;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut roboter_arm = robot::RobotArm::new("192.168.2.40:30002").await?;
+    // Geschwindigkeiten der einzelnen Gelenke festlegen
+    // Zum Testen sicherheitshalber verlangsamt
+    roboter_arm.speedj(0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 1.0, 20.0).await?;
+    //Auf bestimmte Koordinaten fahren
+    // NOCH NICHT GETESTET
     roboter_arm.movel(1.0, 1.0, 1.0, 0.0, 0.0, 0.0, None, None).await?;
     Ok(())
 }
