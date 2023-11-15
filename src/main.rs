9081d656b6bb26cd1cdd69b3d84af06cc9f744d0
Max <maxsunkovsky@gmail.com>
2023-11-15 12:59:38 +0100

    first test withnew struct

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ mod webserver;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(async {
         let mut roboter_arm = robot::RobotArm::new("192.168.2.40:30002").await.unwrap();
-        roboter_arm
-            .movel(1.0, 1.0, 1.0, 0.0, 0.0, 0.0, None, None)
-            .await
-            .unwrap();
+        roboter_arm.movel(-245.85, -857.72, 199.7, 1.211, 2.896, 0.012, None, None).await.unwrap();
+            
     });
 
     webserver::start_webserver().await.unwrap();
