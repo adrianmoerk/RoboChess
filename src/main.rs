d8863fb20cf058e663fd225573c8bb805149684b
Adrian Mörk <phantomenias@protonmail.com>
2023-11-29 09:52:22 +0100

    Update roboter_arm IP address

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let test_field = ChessTilePosition::new_position('c', 3);
         test_field.convert_pos_to_coords();
         // let mut roboter_arm = robot::RobotArm::new("192.168.2.40:30002").await.unwrap();
-        let mut roboter_arm = robot::RobotArm::new("ur-2018300294:30002").await.unwrap();
-
+        // let mut roboter_arm = robot::RobotArm::new("ur-2018300294:30002").await.unwrap();
+        let mut roboter_arm = robot::RobotArm::new("129.143.38.67:30002").await.unwrap();
 
         let command = String::from("get_tcp_offset()");
         roboter_arm
@@ -22,7 +22,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         roboter_arm.stream.read_to_string(&mut buf).await.unwrap();
         println!("{}", buf);
 
-        
         roboter_arm
             .movel(-245.85, -857.72, 199.7, 1.211, 2.896, 0.012, None, None)
             .await
