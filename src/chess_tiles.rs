c69421b5233597de8f95af7f7698cf8a750634ce
Adrian Mörk <phantomenias@protonmail.com>
2023-12-20 09:52:33 +0100

    gripper debugging setup

--- src/chess_tiles.rs
+++ src/chess_tiles.rs
@@ -3,259 +3,259 @@
 //! This saves the trouble of initializing the chess tiles in the main function.
 #![allow(dead_code)]
 use super::robot::ChessTilePosition;
-const CHESS_TILE_POS_A1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A1: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 1,
 };
-const CHESS_TILE_POS_A2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A2: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 2,
 };
-const CHESS_TILE_POS_A3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A3: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 3,
 };
-const CHESS_TILE_POS_A4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A4: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 4,
 };
-const CHESS_TILE_POS_A5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A5: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 5,
 };
-const CHESS_TILE_POS_A6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A6: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 6,
 };
-const CHESS_TILE_POS_A7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A7: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 7,
 };
-const CHESS_TILE_POS_A8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_A8: ChessTilePosition = ChessTilePosition {
     field_char: 'a',
     field_num: 8,
 };
-const CHESS_TILE_POS_B1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B1: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 1,
 };
-const CHESS_TILE_POS_B2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B2: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 2,
 };
-const CHESS_TILE_POS_B3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B3: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 3,
 };
-const CHESS_TILE_POS_B4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B4: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 4,
 };
-const CHESS_TILE_POS_B5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B5: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 5,
 };
-const CHESS_TILE_POS_B6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B6: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 6,
 };
-const CHESS_TILE_POS_B7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B7: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 7,
 };
-const CHESS_TILE_POS_B8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_B8: ChessTilePosition = ChessTilePosition {
     field_char: 'b',
     field_num: 8,
 };
-const CHESS_TILE_POS_C1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C1: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 1,
 };
-const CHESS_TILE_POS_C2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C2: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 2,
 };
-const CHESS_TILE_POS_C3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C3: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 3,
 };
-const CHESS_TILE_POS_C4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C4: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 4,
 };
-const CHESS_TILE_POS_C5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C5: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 5,
 };
-const CHESS_TILE_POS_C6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C6: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 6,
 };
-const CHESS_TILE_POS_C7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C7: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 7,
 };
-const CHESS_TILE_POS_C8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_C8: ChessTilePosition = ChessTilePosition {
     field_char: 'c',
     field_num: 8,
 };
-const CHESS_TILE_POS_D1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D1: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 1,
 };
-const CHESS_TILE_POS_D2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D2: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 2,
 };
-const CHESS_TILE_POS_D3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D3: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 3,
 };
-const CHESS_TILE_POS_D4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D4: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 4,
 };
-const CHESS_TILE_POS_D5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D5: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 5,
 };
-const CHESS_TILE_POS_D6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D6: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 6,
 };
-const CHESS_TILE_POS_D7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D7: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 7,
 };
-const CHESS_TILE_POS_D8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_D8: ChessTilePosition = ChessTilePosition {
     field_char: 'd',
     field_num: 8,
 };
-const CHESS_TILE_POS_E1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E1: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 1,
 };
-const CHESS_TILE_POS_E2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E2: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 2,
 };
-const CHESS_TILE_POS_E3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E3: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 3,
 };
-const CHESS_TILE_POS_E4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E4: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 4,
 };
-const CHESS_TILE_POS_E5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E5: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 5,
 };
-const CHESS_TILE_POS_E6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E6: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 6,
 };
-const CHESS_TILE_POS_E7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E7: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 7,
 };
-const CHESS_TILE_POS_E8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_E8: ChessTilePosition = ChessTilePosition {
     field_char: 'e',
     field_num: 8,
 };
-const CHESS_TILE_POS_F1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F1: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 1,
 };
-const CHESS_TILE_POS_F2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F2: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 2,
 };
-const CHESS_TILE_POS_F3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F3: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 3,
 };
-const CHESS_TILE_POS_F4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F4: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 4,
 };
-const CHESS_TILE_POS_F5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F5: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 5,
 };
-const CHESS_TILE_POS_F6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F6: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 6,
 };
-const CHESS_TILE_POS_F7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F7: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 7,
 };
-const CHESS_TILE_POS_F8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_F8: ChessTilePosition = ChessTilePosition {
     field_char: 'f',
     field_num: 8,
 };
-const CHESS_TILE_POS_G1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G1: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 1,
 };
-const CHESS_TILE_POS_G2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G2: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 2,
 };
-const CHESS_TILE_POS_G3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G3: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 3,
 };
-const CHESS_TILE_POS_G4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G4: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 4,
 };
-const CHESS_TILE_POS_G5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G5: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 5,
 };
-const CHESS_TILE_POS_G6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G6: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 6,
 };
-const CHESS_TILE_POS_G7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G7: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 7,
 };
-const CHESS_TILE_POS_G8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_G8: ChessTilePosition = ChessTilePosition {
     field_char: 'g',
     field_num: 8,
 };
-const CHESS_TILE_POS_H1: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H1: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 1,
 };
-const CHESS_TILE_POS_H2: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H2: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 2,
 };
-const CHESS_TILE_POS_H3: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H3: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 3,
 };
-const CHESS_TILE_POS_H4: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H4: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 4,
 };
-const CHESS_TILE_POS_H5: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H5: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 5,
 };
-const CHESS_TILE_POS_H6: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H6: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 6,
 };
-const CHESS_TILE_POS_H7: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H7: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 7,
 };
-const CHESS_TILE_POS_H8: ChessTilePosition = ChessTilePosition {
+pub const CHESS_TILE_POS_H8: ChessTilePosition = ChessTilePosition {
     field_char: 'h',
     field_num: 8,
 };
--- src/main.rs
+++ src/main.rs
@@ -12,51 +12,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(async {
         let mut roboter_arm = robot::RobotArm::new("192.168.2.40:30002").await.unwrap();
         tokio::time::sleep(Duration::from_secs(5)).await;
-        let mut ws = WebSocket::connect("ws://127.0.0.1:8080/ws")
+        roboter_arm
+            .move_chesspiece_to_empty_field(
+                &chess_tiles::CHESS_TILE_POS_A1,
+                &chess_tiles::CHESS_TILE_POS_B3,
+            )
             .await
-            .expect("Failed to Connect to Websocket");
-        ws.send_text("/join backend".to_string()).await.unwrap();
+            .unwrap();
 
-        loop {
-            let gui_command = ws.receive().await.unwrap().as_text().unwrap().0.to_string();
-            println!("received command: {:?}", gui_command);
-            let gui_command_vec = gui_command.split(" ").collect::<Vec<&str>>();
-            let from_field_str = gui_command_vec.get(0).unwrap().to_string();
-            let move_operation_str = gui_command_vec.get(1).unwrap().to_string();
-            let to_field_str = gui_command_vec.get(2).unwrap().to_string();
-            let from_field = ChessTilePosition::position_from_str(from_field_str);
-            let to_field = ChessTilePosition::position_from_str(to_field_str);
+        // COMMENT IN THIS LOGIC TO BIND THIS TO THE GUI
+        // let mut ws = WebSocket::connect("ws://127.0.0.1:8080/ws")
+        //     .await
+        //     .expect("Failed to Connect to Websocket");
+        // ws.send_text("/join backend".to_string()).await.unwrap();
 
-            match move_operation_str.as_str() {
-                "to" => {
-                    roboter_arm
-                        .move_chesspiece_to_empty_field(&from_field, &to_field)
-                        .await
-                        .unwrap();
-                }
-                "hit" => roboter_arm
-                    .move_chesspiece_to_occupied_field(&from_field, &to_field)
-                    .await
-                    .unwrap(),
-                _ => {}
-            }
-        }
-
-        // let a1 = ChessTilePosition::new_position('a', 1);
-        // let a8 = ChessTilePosition::new_position('a', 8);
-        // let h8 = ChessTilePosition::new_position('h', 8);
-        // let h1 = ChessTilePosition::new_position('h', 1);
-
-        // roboter_arm.open_gripper().await.unwrap();
-
-        // tokio::time::sleep(Duration::from_secs(8)).await;
-        // roboter_arm.move_to_field(&a1, None, None).await.unwrap();
-        // tokio::time::sleep(Duration::from_secs(8)).await;
-        // roboter_arm.move_to_field(&a8, None, None).await.unwrap();
-        // tokio::time::sleep(Duration::from_secs(8)).await;
-        // roboter_arm.move_to_field(&h8, None, None).await.unwrap();
-        // tokio::time::sleep(Duration::from_secs(8)).await;
-        // roboter_arm.move_to_field(&h1, None, None).await.unwrap();
+        // loop {
+        //     let gui_command = ws.receive().await.unwrap().as_text().unwrap().0.to_string();
+        //     println!("received command: {:?}", gui_command);
+        //     let gui_command_vec = gui_command.split(" ").collect::<Vec<&str>>();
+        //     let from_field_str = gui_command_vec.get(0).unwrap().to_string();
+        //     let move_operation_str = gui_command_vec.get(1).unwrap().to_string();
+        //     let to_field_str = gui_command_vec.get(2).unwrap().to_string();
+        //     let from_field = ChessTilePosition::position_from_str(from_field_str);
+        //     let to_field = ChessTilePosition::position_from_str(to_field_str);
+        //     match move_operation_str.as_str() {
+        //         "to" => {
+        //             roboter_arm
+        //                 .move_chesspiece_to_empty_field(&from_field, &to_field)
+        //                 .await
+        //                 .unwrap();
+        //         }
+        //         "hit" => roboter_arm
+        //             .move_chesspiece_to_occupied_field(&from_field, &to_field)
+        //             .await
+        //             .unwrap(),
+        //         _ => {}
+        //     }
+        // }
     });
     webserver::start_webserver().await.unwrap();
     Ok(())
